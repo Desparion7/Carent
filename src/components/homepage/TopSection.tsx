865b1ec116660ffc50ffda70a432791f75de9aaa
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-06-03 14:20:28 +0200

    change video

--- src/components/homepage/TopSection.tsx
+++ src/components/homepage/TopSection.tsx
@@ -3,8 +3,8 @@ import styles from './TopSection.module.scss';
 const TopSection = () => {
   return (
     <div className={`${styles.topSection} `}>
-      <h1>Rent Your Dream Car&apos;s With Us</h1>
       <div className={styles.car3d}>
+        <h1>Rent Your Dream Car&apos;s With Us</h1>
         <video className={styles.video} src="audi.mp4" autoPlay muted />
       </div>
     </div>
