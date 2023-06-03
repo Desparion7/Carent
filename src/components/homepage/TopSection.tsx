efd992ec7280814d25c30fc4d828dea0ee192f91
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-06-03 15:20:52 +0200

    add text animation

--- src/components/homepage/TopSection.tsx
+++ src/components/homepage/TopSection.tsx
@@ -4,7 +4,7 @@ const TopSection = () => {
   return (
     <div className={`${styles.topSection} `}>
       <div className={styles.car3d}>
-        <h1>Rent Your Dream Car&apos;s With Us</h1>
+        <h1 className={styles.showText}>Rent Your Dream Car&apos;s With Us</h1>
         <video className={styles.video} src="audi.mp4" autoPlay muted />
       </div>
     </div>
