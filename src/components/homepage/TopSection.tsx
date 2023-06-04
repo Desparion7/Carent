feae095589e63bd772f496377471f5723b11eb88
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-06-04 17:03:07 +0200

    change video

--- src/components/homepage/TopSection.tsx
+++ src/components/homepage/TopSection.tsx
@@ -5,7 +5,7 @@ const TopSection = () => {
     <div className={`${styles.topSection} `}>
       <div className={styles.car3d}>
         <h1 className={styles.showText}>Rent Your Dream Car&apos;s With Us</h1>
-        <video className={styles.video} src="audi.mp4" autoPlay muted />
+        <video className={styles.video} src="car.mp4" autoPlay muted />
       </div>
     </div>
   );
