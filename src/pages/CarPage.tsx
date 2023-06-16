f122734519cbde1c67fbfd9bb04c67807edbba95
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-06-16 16:27:08 +0200

    change img preloaded

--- src/pages/CarPage.tsx
+++ src/pages/CarPage.tsx
@@ -1,10 +1,11 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import cars from '../data/cars';
 import styles from './CarPage.module.scss';
 import CarParameters from '../components/carPage/CarParameters';
 import CarDescription from '../components/carPage/CarDescription';
 import CarInfoNavigation from '../components/carPage/CarInfoNavigation';
+import LoadingSpinner from '../components/ui/LoadingSpinner';
 
 const CarPage = () => {
   const param = useParams();
@@ -13,43 +14,52 @@ const CarPage = () => {
 
   const carInfo = cars.find((car) => car.name === newCarName);
 
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   useEffect(() => {
     const animation = new Image();
     animation.src = carInfo?.img[1] as string;
     animation.onload = () => {};
+    setImgLoaded(true);
   }, [carInfo]);
+  let content;
 
-  return (
-    <div className={styles.carPage}>
-      <div>
-        <img src={carInfo?.img[1]} alt={carInfo?.name} />
-      </div>
-      <div className={styles.carPage__container}>
-        <div className={styles.carPage__container__info}>
-          <CarParameters
-            name={carInfo?.name}
-            power={carInfo?.power}
-            acceleration={carInfo?.acceleration}
-            drivetrain={carInfo?.drivetrain}
-            engine={carInfo?.engine}
-            gearType={carInfo?.gearType}
-            max={carInfo?.max}
-            torque={carInfo?.torque}
-            seats={carInfo?.seats}
-            img={carInfo?.img}
-            dailyPrice={carInfo?.dailyPrice}
-          />
-          <CarDescription description={carInfo?.description} />
-          <CarDescription description={carInfo?.description} />
-          <CarDescription description={carInfo?.description} />
-          <CarDescription description={carInfo?.description} />
-          <CarDescription description={carInfo?.description} />
-          <CarDescription description={carInfo?.description} />
+  if (imgLoaded) {
+    content = (
+      <div className={styles.carPage}>
+        <div>
+          <img src={carInfo?.img[1]} alt={carInfo?.name} />
         </div>
-        <CarInfoNavigation dailyPrice={carInfo?.dailyPrice} />
+        <div className={styles.carPage__container}>
+          <div className={styles.carPage__container__info}>
+            <CarParameters
+              name={carInfo?.name}
+              power={carInfo?.power}
+              acceleration={carInfo?.acceleration}
+              drivetrain={carInfo?.drivetrain}
+              engine={carInfo?.engine}
+              gearType={carInfo?.gearType}
+              max={carInfo?.max}
+              torque={carInfo?.torque}
+              seats={carInfo?.seats}
+              img={carInfo?.img}
+              dailyPrice={carInfo?.dailyPrice}
+            />
+            <CarDescription description={carInfo?.description} />
+            <CarDescription description={carInfo?.description} />
+            <CarDescription description={carInfo?.description} />
+            <CarDescription description={carInfo?.description} />
+            <CarDescription description={carInfo?.description} />
+            <CarDescription description={carInfo?.description} />
+          </div>
+          <CarInfoNavigation dailyPrice={carInfo?.dailyPrice} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    content = <LoadingSpinner />;
+  }
+  return content;
 };
 
 export default CarPage;
