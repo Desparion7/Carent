9fef11c364ff3784f1b72e9c7f19fc1ce40f1a8c
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-07-13 13:02:28 +0200

    add about us text

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,7 +1,57 @@
 import styles from './AboutUsPage.module.scss';
 
 const AboutUsPage = () => {
-  return <div className={styles.aboutus}>lorem</div>;
+  return (
+    <div className={`${styles.aboutus} wrapper`}>
+      <h2>About us</h2>
+      <p>
+        Welcome to Carrent, the premier destination for luxury car rentals. With
+        a passion for excellence and a commitment to exceptional service, we are
+        dedicated to providing you with an unforgettable driving experience.
+      </p>
+      <p>
+        At Carrent, we understand that your journey is about more than just
+        getting from point A to point B. It&apos;s about indulging in the thrill
+        of the open road, reveling in the elegance of a finely crafted vehicle,
+        and creating memories that will last a lifetime. That&apos;s why we
+        offer a meticulously curated fleet of high-end automobiles that combine
+        power, style, and sophistication.
+      </p>
+      <p>
+        Our collection of luxury cars includes iconic brands like BMW,
+        Mercedes-Benz, Audi, and more. Each vehicle is meticulously maintained
+        and regularly serviced to ensure optimal performance and your utmost
+        satisfaction. From sleek sedans and stylish convertibles to spacious
+        SUVs and powerful sports cars, we have the perfect vehicle to suit your
+        discerning tastes and meet your specific needs.
+      </p>
+      <p>
+        At Carrent, we pride ourselves on delivering a seamless and personalized
+        experience from start to finish. Our dedicated team of professionals is
+        here to assist you every step of the way, from selecting the ideal
+        vehicle to arranging convenient pickup and drop-off options. We strive
+        to exceed your expectations by providing exceptional customer service
+        and ensuring that your rental process is effortless and enjoyable.
+      </p>
+      <p>
+        Whether you&apos;re looking to impress at a special event, explore a new
+        city in style, or simply indulge in the pleasure of driving a luxury
+        car, Carrent is here to make it happen. With our competitive rates,
+        flexible rental periods, and a commitment to excellence, we are your
+        trusted partner in luxury car rentals.
+      </p>
+      <p>
+        Discover the joy of driving a world-class vehicle. Experience the
+        unparalleled elegance and performance of a luxury car. Choose Carrent
+        for an extraordinary journey that begins the moment you step behind the
+        wheel.
+      </p>
+      <p>
+        Book your dream ride with Carrent today and let us elevate your driving
+        experience to new heights.
+      </p>
+    </div>
+  );
 };
 
 export default AboutUsPage;
